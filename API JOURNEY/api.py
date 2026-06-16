7b49fb6682695646c5548df707008f506486027f
Arsal Ahmad <ahmadarsal217@gmail.com>
2026-06-17 00:23:26 +0500

    API Journey: added greet, age, location endpoints

--- API JOURNEY/api.py
+++ API JOURNEY/api.py
@@ -12,3 +12,17 @@ def about():
 @app.get("/skills")
 def skills():
     return {"skills": ["Python", "Dictionaries", "File Handling", "FastAPI"]}
+
+ #/greet → returns {"message": "Welcome to my API"}
+
+@app.get("/greet")
+def greet():
+    return {"message": "Welcome to my API"}
+
+@app.get("/age")
+def age():
+    return {"age": 25}
+
+@app.get("/location")
+def location():
+    return {"Country": "Pakistan", "City": "Karachi"}
